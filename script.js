53eb714e0443bd6c9d067b345e1c212117787170
David Ornellas <ornellasd@gmail.com>
2017-08-28 16:55:15 -0700

    added player icon

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 var playerScore = 0;
 var computerScore = 0;
 
+var resultImg = document.getElementById("derp-image");
+
+var iconArray = [
+  "img/rock.png",
+  "img/paper.png",
+  "img/scissors.png"
+]
+
 function randomNumber(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
 function playerWin() {
-		playerScore++;
-		document.getElementById("player-score").innerHTML = ("Your Score: " + playerScore );
+  playerScore++;
+  document.getElementById("player-score").innerHTML = playerScore;
 }
 
 function computerWin() {
-	computerScore++;
-	document.getElementById("computer-score").innerHTML = ("A.I. Score: " + computerScore)
+  computerScore++;
+  document.getElementById("computer-score").innerHTML = computerScore;
 }
 
 function roShamBo() {
- 
+
   var player = prompt("rock (r), paper (p) or scissors (s)?");
   var computTard = Math.floor(randomNumber(1, 4));
-  
-  
+
+
   //player input
   if (player === "r") {
     var playerChoice = "rock";
+    resultImg.src = iconArray[0];
   } else if (player === "p") {
     var playerChoice = "paper";
+    resultImg.src = iconArray[1];
   } else if (player === "s") {
     var playerChoice = "scissors";
+    resultImg.src= iconArray[2];
   } else {
     var playerChoice = "ERROR";
   }
@@ -42,7 +53,7 @@ function roShamBo() {
     var computerChoice = "scissors";
   }
 
-	//Game logic
+  //Game logic
   if (playerChoice === "rock" && computerChoice === "paper") {
     var result = "computer wins";
     computerWin();
@@ -68,8 +79,3 @@ function roShamBo() {
   document.getElementById("derp").innerHTML = ("Match: " + playerChoice + " vs. " + computerChoice);
   document.getElementById("outcome").innerHTML = ("Result: " + result);
 }
-
-
-
-
-
